248be9b4e612fe3bdce9a1ba3d9cddc27af80ff0
A1bemuth <albemuth1221@gmail.com>
2014-11-28 13:28:45 +0500

    Decomposed #1

--- CleanCode/01_ExtractMethods.cs
+++ CleanCode/01_ExtractMethods.cs
@@ -7,24 +7,16 @@ namespace CleanCode
 	{
 		private static void SaveData(string s, byte[] d)
 		{
-			//open files
-			var fs1 = new FileStream(s, FileMode.OpenOrCreate);
-			var fs2 = new FileStream(Path.ChangeExtension(s, "bkp"), FileMode.OpenOrCreate);
-
-			// write data
-			fs1.Write(d, 0, d.Length);
-			fs2.Write(d, 0, d.Length);
-
-			// close files
-			fs1.Close();
-			fs2.Close();
-
-			// save last-write time
-			string tf = s + ".time";
-			var fs3 = new FileStream(tf, FileMode.OpenOrCreate);
-			var t = BitConverter.GetBytes(DateTime.Now.Ticks);
-			fs3.Write(t, 0, t.Length);
-			fs3.Close();
+            WriteToFileStream(s,d);
+            WriteToFileStream(Path.ChangeExtension(s, "bkp"), d);
+            WriteToFileStream(s + ".time", BitConverter.GetBytes(DateTime.Now.Ticks));
 		}
+
+	    private static void WriteToFileStream(string path, byte[] data)
+	    {
+	        var fs = new FileStream(path, FileMode.OpenOrCreate);
+            fs.Write(data, 0, data.Length);
+            fs.Close();
+	    }
 	}
 }

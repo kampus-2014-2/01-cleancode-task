936da95bae7bb1a9097d172cc65f118a07801e10
Kuga <sidorov.sergey.n@gmail.com>
2014-11-28 14:34:22 +0600

    Change

--- CleanCode/01_ExtractMethods.cs
+++ CleanCode/01_ExtractMethods.cs
@@ -5,26 +5,28 @@ namespace CleanCode
 {
 	public static class RefactorMethod
 	{
-		private static void SaveData(string s, byte[] d)
+	    /// <summary>
+	    /// Open file.
+	    /// </summary>
+	    /// <param name="file">File name</param>
+	    /// <param name="data">Data</param>
+	    /// <returns>Stream</returns>
+	    private static void WriteData(string file, byte[] data)
+	    {
+            var stream = new FileStream(file, FileMode.OpenOrCreate);
+            stream.Write(data, 0, data.Length);
+            stream.Close();
+	    }
+
+       private static void SaveData(string s, byte[] d)
 		{
-			//open files
-			var fs1 = new FileStream(s, FileMode.OpenOrCreate);
-			var fs2 = new FileStream(Path.ChangeExtension(s, "bkp"), FileMode.OpenOrCreate);
-
-			// write data
-			fs1.Write(d, 0, d.Length);
-			fs2.Write(d, 0, d.Length);
-
-			// close files
-			fs1.Close();
-			fs2.Close();
+			WriteData(s, d);
+            WriteData(Path.ChangeExtension(s, "bkp"), d);
 
 			// save last-write time
 			string tf = s + ".time";
-			var fs3 = new FileStream(tf, FileMode.OpenOrCreate);
-			var t = BitConverter.GetBytes(DateTime.Now.Ticks);
-			fs3.Write(t, 0, t.Length);
-			fs3.Close();
+            var t = BitConverter.GetBytes(DateTime.Now.Ticks);
+            WriteData(tf, d);
 		}
 	}
 }
